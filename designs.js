9ecbd68118f3adb5fa82b7e32d1c1780aa2942c8
haregbirhane <haregbirhane1@gmail.com>
2023-04-19 17:50:01 -0500

    feat - code completed

--- designs.js
+++ designs.js
@@ -1,10 +1,39 @@
 // Select color input
+const colorPicker = document.getElementById('colorPicker');
+
 // Select size input
+const sizePicker = document.getElementById('sizePicker');
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+
+// Select the table
+const pixelCanvas = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function (event) {
+  event.preventDefault();
+  makeGrid();
+});
 
+// Create the grid
 function makeGrid() {
+  // Clear the previous grid
+  pixelCanvas.innerHTML = '';
 
-// Your code goes here!
+  // Get the input values for height and width
+  const height = inputHeight.value;
+  const width = inputWidth.value;
 
+  // Create the grid using nested loops
+  for (let i = 0; i < height; i++) {
+    const row = pixelCanvas.insertRow(i);
+    for (let j = 0; j < width; j++) {
+      const cell = row.insertCell(j);
+
+      // Add an event listener to each cell for coloring
+      cell.addEventListener('click', function () {
+        cell.style.backgroundColor = colorPicker.value;
+      });
+    }
+  }
 }
